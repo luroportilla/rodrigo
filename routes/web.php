77386bd0c68b26a5e0b4e10f6c8c4ddb0c5e8ac9
Rodrigo Portilla <luroportilla300188@gmail.com>
2024-08-28 15:26:40 -0500

    segundo commit de prueba

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/admin', function () {
     return view('admin.index');
 });
 
+Route::get('/admin', function () {
+    return view('admin.index');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
